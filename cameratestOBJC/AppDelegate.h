170bbfc7fcc3659d53049ebb248792e43a32ed13
HsHack <SiliconHacks@e1z1r8p5.local>
2017-05-06 21:49:20 -0700

    Initial Commit

---
+++ cameratestOBJC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cameratestOBJC
+//
+//  Created by Silicon Hacks on 5/6/17.
+//  Copyright © 2017 Silicon Hacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cameratestOBJC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cameratestOBJC
+//
+//  Created by Silicon Hacks on 5/6/17.
+//  Copyright © 2017 Silicon Hacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
